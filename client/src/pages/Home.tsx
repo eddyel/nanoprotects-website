39e4d5bdfe48bdad361363b9e1e4189a8fe133f6
eddyel <81652227+eddyel@users.noreply.github.com>
2026-03-16 17:47:59 +0000

    Update Home.tsx
    backgroundImage: 'url(/images/hero-riad-marrakech.webp)',

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <section 
         className="relative h-screen flex flex-col items-center justify-between w-full pt-20"
         style={{
-          backgroundImage: 'url(https://files.manuscdn.com/user_upload_by_module/session_file/310419663028302117/UxxaIQUWeWFAiEWn.webp)',
+          backgroundImage: 'url(/images/hero-riad-marrakech.webp)',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundAttachment: 'fixed'
